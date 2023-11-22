d5613fb9296da81350b657f52d096a67accefd65
LeonardoCarmo561 <carmoleo561@gmail.com>
2023-11-22 16:48:35 -0300

    update redirect

--- src/app/(admin_routes)/admin/page.tsx
+++ src/app/(admin_routes)/admin/page.tsx
@@ -1,6 +1,7 @@
 import { AdminLoginForm } from '@/components/pages/admin'
 
 import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Administração · TickDoc',
@@ -8,7 +9,9 @@ export const metadata: Metadata = {
 }
 
 export default function LoginAdminPages() {
-  return (
+  return process.env.NODE_ENV === 'production' ? (
+    redirect('/admin')
+  ) : (
     <main className="flex min-h-screen bg-gradient-to-br from-blue-300 to bg-blue-500 justify-center items-center">
       <AdminLoginForm />
     </main>
