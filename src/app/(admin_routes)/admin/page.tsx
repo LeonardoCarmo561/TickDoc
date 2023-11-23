7f3acdd4c4f1053d03b2f021da9526b3ad9bc6a7
LeonardoCarmo561 <carmoleo561@gmail.com>
2023-11-23 08:46:11 -0300

    fix: infinity redirect in login page

--- src/app/(admin_routes)/admin/page.tsx
+++ src/app/(admin_routes)/admin/page.tsx
@@ -1,7 +1,6 @@
 import { AdminLoginForm } from '@/components/pages/admin'
 
 import { Metadata } from 'next'
-import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Administração · TickDoc',
@@ -9,9 +8,7 @@ export const metadata: Metadata = {
 }
 
 export default function LoginAdminPages() {
-  return process.env.NODE_ENV === 'production' ? (
-    redirect('/admin')
-  ) : (
+  return (
     <main className="flex min-h-screen bg-gradient-to-br from-blue-300 to bg-blue-500 justify-center items-center">
       <AdminLoginForm />
     </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
+  return process.env.NODE_ENV === 'production' ? (
+    redirect('/admin')
+  ) : (
     <main>
       <h1>Página de vendas do TickDoc</h1>
     </main>
