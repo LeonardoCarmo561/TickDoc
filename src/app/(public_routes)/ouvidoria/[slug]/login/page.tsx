2d8dbf5b5ec7fd517ea217fbb8e5bc49ad531df1
LeonardoCarmo561 <carmoleo561@gmail.com>
2023-12-07 13:43:29 -0300

    add new external user page

--- src/app/(public_routes)/ouvidoria/[slug]/login/page.tsx
+++ src/app/(public_routes)/ouvidoria/[slug]/login/page.tsx
@@ -1,11 +1,32 @@
-import { Editor } from '@/components/editor'
+import { ClientData } from '@/@types'
+import { Environment } from '@/utils'
+import { Metadata, ResolvingMetadata } from 'next'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+  parent: ResolvingMetadata
+}): Promise<Metadata> {
+  const ombudsmanSlug = params.slug
+  const institution: ClientData = await fetch(
+    `${
+      Environment.NODE_ENV === 'development'
+        ? 'http://127.0.0.1:8000'
+        : Environment.URL_BASE
+    }/V1/institution/${ombudsmanSlug}/`,
+  ).then((res) => res.json())
+
+  return {
+    title: `${institution.slug} · ${institution.ombudsman_title} · Ouvidoria · TickDoc`,
+    description: `${institution.slug} · ${institution.ombudsman_title} · Ouvidoria Eletrônica. Acesse e abra sua manifestação de maneira rápida e fácil · TickDoc`,
+  }
+}
 
 export default function OmbudsmanLoginPage() {
   return (
     <main>
-      <span>
-        <Editor />
-      </span>
+      <span>opa</span>
     </main>
   )
 }
--- src/utils/environment/index.ts
+++ src/utils/environment/index.ts
@@ -1,4 +1,5 @@
 export const Environment = {
   URL_BASE: process.env.NEXT_PUBLIC_BASE_URL,
   CUSTOMER_SERVICE_URL: process.env.NEXT_PUBLIC_CUSTOMER_SERVICE_URL,
+  NODE_ENV: process.env.NODE_ENV,
 }
